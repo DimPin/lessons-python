71a072336133088f4a193925e96809b36ba1cba3
Dima Pinaev <dimapokemon005@gmail.com>
2022-12-18 20:04:39 +0300

    homework-1

--- homework-1/python.py
+++ homework-1/python.py
@@ -1,3 +1,5 @@
+import math
+
 # Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
 # является ли этот день выходным.
 
@@ -29,3 +31,49 @@ isTrueState(1, 0, 0)
 isTrueState(1, 0, 1)
 isTrueState(1, 1, 0)
 isTrueState(1, 1, 1)
+
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
+# выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+
+x = int(input('Input x: '))
+y = int(input('Input y: '))
+
+if x != 0 and y != 0:
+    if x > 0 and y > 0:
+        print(1)
+    elif x > 0 and y < 0:
+        print(4)
+    elif x < 0 and y > 0:
+        print(2)
+    elif x < 0 and y < 0:
+        print(3)
+else:
+    print('x or y = 0')
+
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат 
+# точек в этой четверти (x и y).
+
+num_quarter = int(input('Input number: '))
+
+if num_quarter > 0 and num_quarter < 5:
+    if num_quarter == 1:
+        print('x > 0, y > 0')
+    if num_quarter == 2:
+        print('x < 0, y > 0')
+    if num_quarter == 3:
+        print('x < 0, y < 0')
+    if num_quarter == 4:
+        print('x > 0, y < 0')
+else:
+    print('Its not number quarter')
+
+# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между 
+# ними в 2D пространстве.
+
+x1 = int(input('Input x1: '))
+y1 = int(input('Iput y1: '))
+x2 = int(input('Input x2: '))
+y2 = int(input('Input y2: '))
+
+distance = round(math.sqrt((pow(x2 - x1, 2)) + (pow(y2 - y1, 2))), 2)
+print(distance)
