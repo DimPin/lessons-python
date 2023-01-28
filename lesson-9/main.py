813eb30d58f0a591342b59cd34e8691af9c3e05e
Dima Pinaev <dimapokemon005@gmail.com>
2023-01-28 20:32:27 +0300

    homework-9

--- lesson-9/main.py
+++ lesson-9/main.py
@@ -1,5 +1,5 @@
 from telegram import Bot
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from random import randint
 
 token = '5708454167:AAGp2GFfRpeuSEZF9RTo9O5tEAyanFT3Hq8'
@@ -12,40 +12,16 @@ updater = Updater(token)
 dispatcher = updater.dispatcher
 
 def start(update, context):
-    context.bot.send_message(update.effective_chat.id, "Привет \n Как дела?")
-    return A
-
-def howareu(update, context):
     text = update.message.text
-    if 'хор' in text.lower():
-        context.bot.send_message(update.effective_chat.id,"Я рад, что у вас всё хорошо")
+
+    if 'абв' in text.lower():
+        context.bot.send_message(update.effective_chat.id, text.replace('абв', ''))
     else:
-        context.bot.send_message(update.effective_chat.id,"Не грусти, всё будет отлично")
-    context.bot.send_message(update.effective_chat.id,"Какая у тебя погода?")
-    return B
+        context.bot.send_message(update.effective_chat.id, text)
 
-def weather(update, context):
-    text = update.message.text
-    context.bot.send_message(update.effective_chat.id,"У меня сегодня такая же погода")
-    context.bot.send_message(update.effective_chat.id,"Мне пора бежать, пока!")
-    return ConversationHandler.END
+start_handler = MessageHandler(Filters.text, start)
 
-def cancel (update, context):
-    context.bot.send_message(update.effective_chat.id,"Что то пошло не так")
-
-start_handler = CommandHandler('start', start)
-howareu_handler = MessageHandler(Filters.text, howareu)
-weather_handler = MessageHandler(Filters.text, weather)
-cancel_handler = CommandHandler("cancel", cancel)
-
-conv_handler = ConversationHandler(entry_points=[start_handler],
-                                        states={A: [howareu_handler],
-                                        B: [weather_handler]}, 
-                                        fallbacks= [cancel_handler])
-
-
-dispatcher.add_handler(conv_handler)
-
+dispatcher.add_handler(start_handler)
 
 updater.start_polling()
 updater.idle()
